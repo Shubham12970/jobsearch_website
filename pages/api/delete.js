c7e560682135b5e516de75806a7a7c57d7b8360c
Shubham <shubhamtur@outlook.com>
2023-12-06 20:03:15 +0530

    finally committed2

--- pages/api/delete.js
+++ pages/api/delete.js
@@ -1,12 +1,15 @@
-import clientPromise from "../../lib/mongodb"
+import clientPromise from "../../lib/mongodb" 
+import Cookies from "cookies";
 
 export default async function handler(req, res){
     if(req.method == "POST"){
         const username=req.body['username']
         const client = await clientPromise;
           const db = client.db("Users");
-          await db.collection("Profiles").deleteOne({"Username":username})
-          res.redirect("/success")
+          await db.collection("Profiles").deleteOne({"Username":username});
+          const cookies = new Cookies(req, res)
+           cookies.set('username')
+          res.redirect("/")
     }else{
         res.redirect("/")
     }
--- pages/personal-details.js
+++ pages/personal-details.js
@@ -158,7 +158,7 @@ export default function Personal(username){
             </div>
           </div>
           </div>
-        <button  className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150 flex-end" type="submit">
+          <button  className="absolute right-12  bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150" type="submit">
           Submit
         </button>
       </form>
