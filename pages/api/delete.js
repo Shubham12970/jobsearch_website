fa5933452e233bddc48ec04dfd06e39cbc4e2517
Shubham <shubhamtur@outlook.com>
2023-12-06 19:44:43 +0530

    finally committed

--- pages/api/delete.js
+++ pages/api/delete.js
@@ -3,7 +3,6 @@ import clientPromise from "../../lib/mongodb"
 export default async function handler(req, res){
     if(req.method == "POST"){
         const username=req.body['username']
-        console.log(username)
         const client = await clientPromise;
           const db = client.db("Users");
           await db.collection("Profiles").deleteOne({"Username":username})
