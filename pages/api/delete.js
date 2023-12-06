51c5f0ba5187fbd0ad2d41e3d397937c035b2b38
Shubham <shubhamtur@outlook.com>
2023-12-06 18:42:44 +0530

    final commit

--- pages/api/delete.js
+++ pages/api/delete.js
@@ -1,15 +1,12 @@
 import clientPromise from "../../lib/mongodb"
-import Cookies from "cookies";
 
 export default async function handler(req, res){
     if(req.method == "POST"){
-        const cookies = new Cookies(req, res)
         const username=req.body['username']
         console.log(username)
         const client = await clientPromise;
           const db = client.db("Users");
           await db.collection("Profiles").deleteOne({"Username":username})
-
           res.redirect("/home")
     }else{
         res.redirect("/")
